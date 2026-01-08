e6d60eb7e2e3768290a64f5356de3745e3d031c0
Carolina Sá <117090772+CarolSa03@users.noreply.github.com>
2026-01-08 12:24:29 +0000

    Refactor dark mode initialization in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
-import { useDark, usePreferredDark } from '@vueuse/core'
+import { useDark } from '@vueuse/core'
 import App from './App.vue'
 
 const app = createApp(App)
-usePreferredDark()
-useDark({ value: true, selector: 'html' })
+document.documentElement.classList.add('dark')
+useDark({ selector: 'html' })
+
 app.mount('#app')
