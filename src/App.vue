8f0d4bc530a3d8a85fd5b5e18e8f16343468accc
Carolina Sá <1220623@isep.ipp.pt>
2026-01-11 23:36:22 +0000

    Sort games by date in descending order
    Added sorting to the filteredGames array so that games are displayed with the most recent date first. Also removed an unused Checkbox import.

--- src/App.vue
+++ src/App.vue
@@ -147,7 +147,6 @@ import GameCard from './GameCard.vue'
 import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
 import {
   Select,
@@ -231,6 +230,8 @@ const fetchGames = async () => {
         )
       )
     }
+
+    filteredGames.sort((a, b) => new Date(b.date) - new Date(a.date));
 
     games.value = filteredGames
     apiGames.value = data.api_games || 0
